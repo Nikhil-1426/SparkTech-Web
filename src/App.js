a682254d22bdaee5c29b788b3fe5764e39281881
Nikhil-1426 <parkar.nikhil1426@gmail.com>
2024-08-30 18:49:27 +0530

    Pages Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-// src/App.js
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SignInSignUp from './SignInSignUp'; // Import your sign-in/sign-up component
 import LandingPage from './LandingPage';
+import Services from './Services'; // Import the Services component
+import AboutUs from './AboutUs';
 import './App.css';
-import './firebase'; // Import your landing page component
+import './firebase';
 
 function App() {
   return (
@@ -13,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<SignInSignUp />} />  {/* Signup/Signin page */}
         <Route path="/landing" element={<LandingPage />} />  {/* Landing page */}
+        <Route path="/services" element={<Services />} />  {/* Services page */}
+        <Route path="/about-us" element={<AboutUs />} />  {/* About Us page */}
       </Routes>
     </Router>
   );
